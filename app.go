0167181b47a9ee1a72619bd8e7eb02edbb7a3ec9
Nivetha <gcpcloudplatform@gmail.com>
2025-08-26 10:27:49 +0000

    add

--- app.go
+++ app.go
@@ -1,20 +1,21 @@
 package main
 
 import (
-    "fmt"
-    "net/http"
+	"fmt"
+	"net/http"
 )
 
 func helloHandler(w http.ResponseWriter, r *http.Request) {
-    fmt.Fprintln(w, "Hello, World!")
+	fmt.Fprintln(w, "Hello, World!")
 }
-func main() {
-    http.HandleFunc("/", helloHandler)
 
-    port := ":8080"
-    fmt.Println("Server listening on http://localhost" + port)
-    err := http.ListenAndServe(port, nil)
-    if err != nil {
-        fmt.Println("Error starting server:", err)
-    }
+func main() {
+	http.HandleFunc("/", helloHandler)
+
+	port := ":8080"
+	fmt.Println("Server listening on http://localhost" + port)
+	err := http.ListenAndServe(port, nil)
+	if err != nil {
+		fmt.Println("Error starting server:", err)
+	}
 }
