88f9fe5305faf31ca7702c8c53dd960c715cbb7a
Nivetha <gcpcloudplatform@gmail.com>
2025-09-15 04:52:22 +0000

    add

--- charts/sa.tf
+++ charts/sa.tf
@@ -53,3 +53,70 @@ metadata = {
 network             = "default"
 subnet              = "default"
 service_account_id  = "my-vm-sa"
+
+variable "name"{
+    type = string
+}
+ 
+variable "machine_type"{
+    type = string
+}
+ 
+variable "zone"{
+    type = string
+}
+
+variable "region"{
+    type = string
+}
+
+variable "metadata"{
+    type = map(string)
+}
+
+variable "bootdisk_size"{
+    type = string
+}
+
+variable "disk"{
+    type = string
+}
+
+variable "project_id"{
+    type = string
+}
+
+variable "service_account"{
+    type = string
+}
+
+variable "image"{
+    type = string
+}
+
+variable "network"{
+    type = string
+}
+
+variable "subnet"{
+    type = string
+}
+
+variable "host_project_id"{
+    type = string
+}
+
+variable "deletion_protection"{
+    type = bool
+}
+
+variable "additional_disks" {
+  description = "List of additional persistent disks to attach to the instance"
+  type = list(object({
+    device_name = string
+    mode        = string # READ_WRITE or READ_ONLY
+    disk_type   = string
+    disk_size   = number
+  }))
+  default = []
+}
